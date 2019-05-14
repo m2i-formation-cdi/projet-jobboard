0250ae7c6326d2f96092c1f0477af3938f2b3889
Nazim Hickmet <toto@mail.com>
2019-05-14 14:41:09 +0200

    module de connection à la BD

---
+++ backend/shared/database.js
@@ -0,0 +1,17 @@
+//Importation du module mysql
+const mysql = require('mysql');
+
+//Création d'une connection
+const connection = mysql.createConnection(
+   {
+      host: '172.26.12.134',
+      user: 'root',
+      password: '123'
+   }
+);
+
+//Activation de la base de données
+connection.query('USE jobboard');
+
+//Exportation du module de connection
+module.exports = connection;
