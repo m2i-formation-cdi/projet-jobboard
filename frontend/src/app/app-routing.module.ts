7eab69b129bcaa224e97196e722c0c0f1b8a3cd9
thioune76 <colelenta@gmail.com>
2019-05-15 13:05:26 +0200

    mise a jour

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },  { path: 'search', loadChildren: './search/search.module#SearchPageModule' }
+
 ];
 
 @NgModule({
