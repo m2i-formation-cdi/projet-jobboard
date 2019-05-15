03ced720f7546b572eadb5f7dfb419d8592cb311
thioune76 <colelenta@gmail.com>
2019-05-15 15:46:49 +0200

    MAJ

--- frontend/src/app/search/search.page.ts
+++ frontend/src/app/search/search.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  public credentials = {
+    date_annonce: null,
+    description_annonce: null,
+    salaire_min : null,
+    salaire_max : null,
+    date: null,
+
+    status: 'annonce'
+  }
+  constructor(private user: UserService, private router:Router) { }
 
   ngOnInit() {
   }
 
+  validForm(){
+    this.user.authenticate(this.credentials)
+      .then(
+        ()=> this.router.navigateByUrl('/home') 
+      )
+      .catch(
+        (err)=> console.log(err)
+      )
+  }
+
 }
+
+
