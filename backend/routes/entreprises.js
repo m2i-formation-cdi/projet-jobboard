608d108c8b69537ef3088edd334ac87d0323d599
RORO <RORO@jgmail.com>
2019-05-15 12:05:22 +0200

    Ajout de l'insertion d'une entreprise backend.

--- backend/routes/entreprises.js
+++ backend/routes/entreprises.js
@@ -5,7 +5,33 @@ const db = require('../shared/database');
 const router = express.Router();
 
 router.post('/new', (req, res) => {
-   res.json({ok: true});
+   //Création de la commande SQL d'insertion
+   const sql = "INSERT INTO entreprises SET ?";
+   //Récupération des infos nécessaire pour l'insert
+   const insertEntrepriseData = {
+      raison_social: req.body.raison_social,
+      mdp: req.body.mdp,
+      email: req.body.email,
+      adresse: req.body.adresse,
+      code_postal: req.body.code_postal,
+      ville: req.body.ville,
+      telephone: req.body.telephone,
+      siret: req.body.siret,
+      id_secteur: req.body.id_secteur
+   };
+   //Lancer la requêt à la base de données
+   db.query(
+      sql,
+      insertEntrepriseData,
+      (err) => {
+         //Gestion de la réponse
+         if (err) {
+            res.json({ error: err });
+         } else {
+            res.json({ insert: OK });
+         }
+      });
+
 });
 
 
